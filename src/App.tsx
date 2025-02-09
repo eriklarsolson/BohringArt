5d7ce038f7a8f36ca4077ef15f29946505ccfdb3
Lars Olson <eriklarsolson@yahoo.com>
2025-02-09 15:06:19 -0700

    get site partially working again with mostly upgraded dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import {Route} from "react-router";
 import Routes from "./Routes";
 import {BrowserRouter} from "react-router-dom";
 import {Footer} from "./components/shared/footer";
-import Header from "./components/home/Header";
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'font-awesome/css/font-awesome.min.css';
+import Header from "@/src/components/shared/header/Header";
 
 function App() {
   return (
--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -32,33 +32,6 @@ class Header extends React.Component<any, any> {
     render() {
         return (
             <>
-                {/*<div className="home-banner">*/}
-                {/*    <img className="home-banner" src={banner} width={"100%"} height={"100%"} />*/}
-
-                {/*    <div className="home-text">*/}
-                {/*        <Row style={{display: "block"}}>*/}
-                {/*            <h1>Help us Launch a Telescope</h1>*/}
-                {/*        </Row>*/}
-                {/*        <Row style={{display: "block", textAlign: "left"}}>*/}
-                {/*            <p>*/}
-                {/*                In this practice, you will learn the building of a circuit to build an electric telescope.*/}
-                {/*                After preparing the telescope it the engine will be observed before being launched to observe star life cycles*/}
-                {/*            </p>*/}
-                {/*        </Row>*/}
-                {/*        <Row style={{display: "block"}}>*/}
-                {/*            <Button*/}
-                {/*                href="/activity/circuit-building"*/}
-                {/*                variant="primary"*/}
-                {/*                type="submit"*/}
-                {/*                className={"btn green-button"}*/}
-                {/*                style={{width: "30%",*/}
-                {/*                clipPath: "polygon(0 0, 95% 0, 100% 100%, 5% 100%)"}}>*/}
-                {/*                Start*/}
-                {/*            </Button>*/}
-                {/*        </Row>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-
                 {/*TODO - Need to figure out how to remove clip path on mobile version of site*/}
                 <Container fluid style={{color: "white", padding: 0}}>
                     <Row style={{margin: 0, padding: 0, clipPath: "polygon(0 0, 100% 0, 100% 85%, 0 100%)", backgroundImage:`url(${stellarBackground})`, minHeight: this.state.width > 1000 ? 1000 : 1500}}>
